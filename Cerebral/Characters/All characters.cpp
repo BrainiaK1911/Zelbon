1c32f10f92cef6c53f9894b12e94e513886bd717
rcnobles <36048441+rcnobles@users.noreply.github.com>
2018-07-26 22:35:11 -0700

    Update and rename Cerebral/characters.cpp to Cerebral/Characters/All characters.cpp

---
+++ Cerebral/Characters/All characters.cpp
@@ -0,0 +1,283 @@
+Zelbon 
+
+//Cerebral
+//C++
+#include<iostream>
+using namespace std;
+int main
+{
+//characters
+//create a class/object for each character
+// (Basic) name,number, World/Verse, Alter-Ego, Age, Gender, Ethnicity/Species, Occupation/Job, Power Rankings, Powers
+// (Appearance) Height, Weight, Skin Tone, Facial Shape, Eye color, Distinguishable 
+//features, Hair style, Body type, posture, Dress, paraphenaelia, accessories 
+
+int 1 = "Abel Ortiz";
+//Abel
+
+int 2 = "Adam Dreton";
+//Atom
+
+int 3 = "Moira Chajolla";
+//Angel
+
+int 4 = "Akell";
+//Akell
+
+int 5 = "Alo";
+//Swadhisthana
+
+int 6 = "Aniah";
+//Aniah the Adamant
+
+int 7 = "Axel
+//Axel
+
+int 8 = "Benjamin"
+//Rage
+
+int 9 = "Brian"
+//Brain
+
+int 10 = "Casey"
+//Casey the Clown
+
+int 11 = "Cayman"
+//Cayman
+
+int 12 = "Chardy Rhein"
+//Chardy
+
+int 13 = "Clark"
+//Mirage
+
+int 14 = "Clyde"
+//Synchro
+
+int 15 = "Danny"
+//Opaque
+
+int 16 = “Dagon”
+//Dragonborn
+
+int 17 = “Damon Jr.”
+//D
+//* Diamond Jack is related to D
+
+int 18 = “Drake”
+//Dracos
+
+int 19 = “E-43”
+//Ebots
+
+Int 20 = “Edge”
+//Edge
+
+Int 21 = “Eli”
+//Elecheetah
+
+Int 22 = “Emma Stone”
+//Emma
+
+Int 23 = “Eric”
+//Block Buster
+
+Int 24 = “Frank”
+//Flux
+
+Int 25 = “Faiz
+//Faiz
+
+Int 26 = “Genkai Rhein”
+//Genkei
+
+Int 27 = “Gia
+//Gia
+
+Int 28 = “Sydney
+//GloChic
+
+Int 29 = “Dr. Porter”
+//Dr. P
+
+Int 30 = “Hugo”
+//Suppressor 
+
+Int 31 = “Harrison”
+// Harry
+
+Int 32 = “Iris”
+//Iris
+
+Int 33 = “Warren Maka”
+//Rhydian
+
+Int 34 = “James”
+//Molecule Man
+
+Int 35 = “Jane Kær”
+//Jane Doe
+
+Int 36 = “Christopher”
+//eRas
+
+Int 37 = “Jeremiah Stone”
+//Outlaw
+
+Int 38 = “John”
+// The Wielder
+
+Int 39 = “Jess”
+//Migrahack
+
+Int 40 = “Kaylin”
+//Kay
+
+Int 41 = “Keah”
+//Keah
+
+Int 42 = “Kevin
+//Kevin
+
+Int 43 = “Klog-Nün”
+//Karl
+
+Int 44 = “Kwame”
+//Kwame
+
+Int 45 = “Phoebus”
+// The Oracle
+
+Int 46 = “Candace Saafir”
+// Ma’at
+
+int 47 = “ Mar-Nun”
+//Martha
+
+int 48  = “Mark”
+//Mark Mega
+
+int 49 = “Mateus”
+//Mateus
+
+int 50 = “Matthew”
+//Magman
+
+int 51 = “Dr.Maxwell”
+//Maximum
+
+int 52 = “Milo”
+//Wrath
+
+int 53 = “Olan”
+//Owl
+
+int 54 = “Nassir”
+//Nas 
+
+int 55 = “Oliver”
+//Oranga 
+
+int 56 = “Perry Snapp”
+//Eli* 
+
+int 57 = “phineas 
+//fin 
+
+int 58 = “prajna Rhein
+//Bodhisattva
+
+int 59 = “Raymond
+//Ray
+
+int 60 = “Rena Rhein “
+//
+
+int 61 = “Rhaja Rhein 
+//Raj
+
+int 62 = “Robert Snapp
+//Tank
+
+int 63 = “Agent Wilson
+//
+
+int 64 = “odd
+//OD 
+
+int 65 = “Rodrick Lawrence 
+//lightning 
+
+int 66 = “Salvatore 
+//
+
+int 67 = “Shawn
+//
+
+int 68 = “Seth
+//
+
+int 69 = “Sophia
+//
+
+int 70 = “Spencer
+//
+
+int 71 = “Talia
+//
+
+int 72 = “Timothy Dreton
+//Tim
+
+int 73 = “Troy
+//Kid
+
+Int 74=” Michael
+//Superstar
+
+Int 75 = “Mokoi
+//Waa Wahn
+
+Int 76 = “Xeven
+
+Int 76 “Petro”
+//Petro the Powerful
+
+Int 77 = “Zeph”
+//Zeph the Adamant
+
+Int 78 = “Scott Keegan
+//John Doe & Mascot
+
+Int 79 = “Kyla
+
+Int 80 = “Aaron”
+//Undretonium miner
+Int 81 = “ Vince”
+//Hotshot
+
+Int 82 = “Dretons
+
+Int 83 = “Ninjax
+
+Int 84 = “Derek”
+//The exterminator
+
+Int 85 = “Kyro”
+//Hermes
+
+Int 86 = “He”
+//watcher, son
+
+Int 87 = “Jacob”
+//Yakub
+
+Int 88 = “Biccho”
+//Dr.Sting
+
+Int 89 = Lochwyne character
+
+Int 90 = Diamond Jack
+
+Int 91 = Grey
+
